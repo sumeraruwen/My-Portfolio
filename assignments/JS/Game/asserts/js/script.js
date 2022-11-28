5a4b90d4760da20fc1456086919761fb9ff35142
unknown <sumeraruwen@gmail.com>
2022-11-28 16:46:31 +0530

    content updated

--- assignments/JS/Game/asserts/js/script.js
+++ assignments/JS/Game/asserts/js/script.js
@@ -41,6 +41,7 @@ function runAnimationStart(){
 
 }
 
+/*
 $(document).on('keydown',function (event){
     if(event.key =="Enter"){
         if(runAnimationNum==0){
@@ -50,5 +51,87 @@ $(document).on('keydown',function (event){
     }
 
 })
+*/
 
 // ========= Background Animation ====================================
+
+var backgroundNum =0;
+
+function backgroundAnimationStart(){
+    backgroundNum=setInterval(backgroundAnimation,120);
+}
+
+var position =0;
+
+function backgroundAnimation(){
+    position = position - 20;
+    $('.background').css("background-position-x",+position+"px")
+
+}
+
+// ========= Jump Animation ====================================
+
+var jumpImgNum =1;
+var jumpAnimationNum =0;
+boytop = 300;
+
+function jumpAnimation(){
+    jumpImgNum =jumpImgNum +1;
+
+    if(jumpImgNum <= 6){
+        boytop = boytop -20;
+        $(".boy").css("top",boytop+"px");
+
+
+
+    }
+    if(jumpImgNum >= 7){
+        boytop = boytop +20;
+        $(".boy").css("top",boytop+"px");
+
+    }
+
+
+    if(jumpImgNum == 13){
+        jumpImgNum = 1;
+        clearInterval(jumpAnimationNum)
+        jumpAnimationNum =0;
+        runImgNumber =0;
+        runAnimationStart()
+
+    }
+
+    boy.src = "asserts/Jump ("+jumpImgNum+").png"
+
+}
+
+function jumpAnimationStart(){
+    clearInterval(idleAnimationNum)
+    runImgNumber=0
+    clearInterval(runAnimationNum)
+    jumpAnimationNum = setInterval(jumpAnimation,100);
+    //clearInterval(runAnimationNum)
+    // clearInterval(idleAnimationNum)
+}
+
+
+// ========= Key Events ====================================
+
+$(document).on('keydown',function (event){
+    if(event.key =="Enter"){
+        if(runAnimationNum==0){
+            runAnimationStart();
+            backgroundAnimationStart();
+        }
+
+    }
+
+    if(event.key =="ArrowUp"){
+        if(jumpAnimationNum ==0){
+            jumpAnimationStart();
+            //   backgroundAnimationStart();
+        }
+
+    }
+
+})
