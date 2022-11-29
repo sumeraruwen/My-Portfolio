4554c975fa4de176c72ec4f146cb01555a4de8f5
unknown <sumeraruwen@gmail.com>
2022-11-29 10:34:33 +0530

    content updated

--- assignments/JS/Game/asserts/js/script.js
+++ assignments/JS/Game/asserts/js/script.js
@@ -1,9 +1,10 @@
+
 var boy = document.getElementById("boyIdle1")
 
-// ========= Idle Animation ====================================
+// ========= Idle Animation Start ====================================
 
-idleImgNum= 0;
-idleAnimationNum = 0;
+var idleImgNum= 0;
+var idleAnimationNum = 0;
 
 function idleAnimation(){
     idleImgNum = idleImgNum + 1;
@@ -20,10 +21,10 @@ function idleAnimationStart(){
 
 }
 
-// ========= Run Animation ====================================
+// ========= Run Animation Start====================================
 
-runImgNumber =0;
-runAnimationNum =0
+var runImgNumber =0;
+var runAnimationNum =0
 
 function runAnimation(){
     runImgNumber = runImgNumber +1 ;
@@ -41,19 +42,8 @@ function runAnimationStart(){
 
 }
 
-/*
-$(document).on('keydown',function (event){
-    if(event.key =="Enter"){
-        if(runAnimationNum==0){
-            runAnimationStart();
-        }
 
-    }
-
-})
-*/
-
-// ========= Background Animation ====================================
+// ========= Background Animation Start================================
 
 var backgroundNum =0;
 
@@ -63,30 +53,34 @@ function backgroundAnimationStart(){
 
 var position =0;
 
+var score =0;
+
 function backgroundAnimation(){
     position = position - 20;
     $('.background').css("background-position-x",+position+"px")
 
+    score++;
+    document.getElementById("score").innerHTML = score;
+
 }
 
-// ========= Jump Animation ====================================
+// ========= Jump Animation Start====================================
 
 var jumpImgNum =1;
 var jumpAnimationNum =0;
-boytop = 300;
+boytop = 347;
 
 function jumpAnimation(){
     jumpImgNum =jumpImgNum +1;
 
     if(jumpImgNum <= 6){
-        boytop = boytop -20;
+        boytop = boytop -70;
         $(".boy").css("top",boytop+"px");
-
 
 
     }
     if(jumpImgNum >= 7){
-        boytop = boytop +20;
+        boytop = boytop +70;
         $(".boy").css("top",boytop+"px");
 
     }
@@ -114,6 +108,79 @@ function jumpAnimationStart(){
     // clearInterval(idleAnimationNum)
 }
 
+// ========= Barriers Animation Start====================================
+
+bMarginLeft=1800;
+
+function createBarriers(){
+
+    for (var i = 0; i <=10 ; i++) {
+
+        $(".background").append("<div style='margin-left:" + bMarginLeft + "px ' id='barrier"+i+"' ></div>");
+        // var barrier = "div"+i;
+
+        // $("#barrier").css("marginLeft",bMarginLeft+"px");
+
+        if(i<5){
+            bMarginLeft = bMarginLeft +3000;
+        }
+        if(i>=5){
+            bMarginLeft = bMarginLeft +1500;
+        }
+    }
+
+
+}
+
+var barrierAnimationId =0;
+
+function barrierAnimation(){
+    for (var i=0;i<=10;i++){
+        var box = document.getElementById("barrier"+i);
+        var currentMargin = getComputedStyle(box).marginLeft;
+        var newMargin = parseInt(currentMargin)-35;
+        box.style.marginLeft = newMargin+"px";
+
+
+        if(newMargin >= -110 & newMargin <=100){
+            if(boytop>300){
+                clearInterval(barrierAnimationId);
+                barrierAnimationId=-1;
+                clearInterval(runAnimationNum);
+                runAnimationNum=-1;
+                clearInterval(jumpAnimationNum);
+                jumpAnimationNum =-1;
+                clearInterval(backgroundNum)
+                backgroundNum=-1;
+
+                setInterval(deadAnimation,100)
+
+            }
+
+        }
+    }
+}
+
+// ========= Dead Animation Start====================================
+
+
+deadImgNum =1;
+
+
+function deadAnimation(){
+    deadImgNum++;
+    if(deadImgNum == 11){
+        deadImgNum =10;
+    }
+
+
+    boy.src = "asserts/Dead ("+deadImgNum+").png"
+
+
+}
+
+
+
 
 // ========= Key Events ====================================
 
@@ -122,6 +189,9 @@ $(document).on('keydown',function (event){
         if(runAnimationNum==0){
             runAnimationStart();
             backgroundAnimationStart();
+        }
+        if(barrierAnimationId ==0){
+            barrierAnimationId = setInterval(barrierAnimation,100)
         }
 
     }
@@ -132,6 +202,13 @@ $(document).on('keydown',function (event){
             //   backgroundAnimationStart();
         }
 
+        if(barrierAnimationId ==0){
+            barrierAnimationId = setInterval(barrierAnimation,100)
+        }
+
     }
 
 })
+
+
+
